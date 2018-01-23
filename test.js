dccf1b0226284e336196a2e3f2944bbf406b71a6
meg768 <meg768@users.noreply.github.com>
2018-01-23 13:53:12 +0100

    -

---
+++ test.js
@@ -0,0 +1,18 @@
+/*
+  "platform" : "Tradfri",
+  "name" : "Tradfri",
+  "debug": true,
+  "coapClientPath": "/usr/local/bin/coap-client",
+  "psk" : "6RnSQ41fP8RdPNan"
+
+*/
+const tradfri = require('node-tradfri').create({
+  coapClientPath: '/usr/local/bin/coap-client', // use embedded coap-client
+  identity: 'IDENTITY',
+  preSharedKey: '6RnSQ41fP8RdPNan',
+  hubIpAddress: '192.168.86.78'
+});
+
+tradfri.getDevices().then((devices) => {
+    console.log(devices);
+});
