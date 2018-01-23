e68470b8d98f33defdd618af9b8bea136d7fd0b7
meg768 <meg768@users.noreply.github.com>
2018-01-23 14:36:07 +0100

    -

--- test.js
+++ test.js
@@ -7,13 +7,37 @@
 
 */
 
-// coap-client -m post -u "Client_identity" -k "6RnSQ41fP8RdPNan" -e '{"9090":"IDENTITY"}' "coaps://192.168.86.78:5684/15011/9063"
+/*
+coap-client -m post -u "Client_identity" -k "6RnSQ41fP8RdPNan" -e '{"9090":"KALLE"}' "coaps://192.168.86.78:5684/15011/9063"
+fS7QW04XZ7im7Lhx
 
-//AKAKMySmHF1Erczp
+coap-client -m post -u "Client_identity" -k "6RnSQ41fP8RdPNan" -e '{"9090":"IKEA"}' "coaps://192.168.86.78:5684/15011/9063"
+{"9091":"bsC0X2H8vX3wLQa3","9029":"1.3.0014"}
 
+
+coap-client -m post -u "Client_identity" -k "6RnSQ41fP8RdPNan" -e '{"9090":"KABEL"}' "coaps://192.168.86.78:5684/15011/9063"
+{"9091":"L7UXBn5ex3xudD1m","9029":"1.3.0014"}
+
+coap-client -m get -u "Client_identity" -k "L7UXBn5ex3xudD1m" "coaps://192.168.86.78:5684/15001"
+/*
+
+/*
+get(type = 'device', id) {
+  const endpoint = type === 'device' ? 15001 : 15004;
+  let command = `${this.coapClientPath} -m get -u "${this.identity}" -k "${this.preSharedKey}" "coaps://${this.ip}:5684/${endpoint}`;
+
+  if (id) {
+    command += `/${id}"`;
+  } else {
+    command += '"';
+  }
+
+  return command;
+}
+*/
 const tradfri = require('node-tradfri').create({
   coapClientPath: '/usr/local/bin/coap-client', // use embedded coap-client
-  identity: 'AKAKMySmHF1Erczp',
+  identity: 'Client_identity',
   preSharedKey: '6RnSQ41fP8RdPNan',
   hubIpAddress: '192.168.86.78'
 });
