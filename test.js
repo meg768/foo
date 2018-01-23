f0e206affa46853514cae51fe02a5cb87418d5cd
meg768 <meg768@users.noreply.github.com>
2018-01-23 14:02:08 +0100

    -

--- test.js
+++ test.js
@@ -6,9 +6,14 @@
   "psk" : "6RnSQ41fP8RdPNan"
 
 */
+
+// coap-client -m post -u "Client_identity" -k "6RnSQ41fP8RdPNan" -e '{"9090":"IDENTITY"}' "coaps://192.168.86.78:5684/15011/9063"
+
+//AKAKMySmHF1Erczp
+
 const tradfri = require('node-tradfri').create({
   coapClientPath: '/usr/local/bin/coap-client', // use embedded coap-client
-  identity: 'IDENTITY',
+  identity: 'AKAKMySmHF1Erczp',
   preSharedKey: '6RnSQ41fP8RdPNan',
   hubIpAddress: '192.168.86.78'
 });
